8689d5545f7528572622909cdf98eebf98618c9c
Sepand <12715168+SepandJP@users.noreply.github.com>
2023-06-06 17:35:05 +0330

    Change the show tasks sorting from ascending to descending.

--- app/models/taskModel.js
+++ app/models/taskModel.js
@@ -1,6 +1,6 @@
 const db = require('../../database/mysql');
 exports.getAll = async() => {
-    const sql = "SELECT `title`, `status` FROM `tasks` ORDER BY `tasks`.`created_at` ASC";
+    const sql = "SELECT `title`, `status` FROM `tasks` ORDER BY `tasks`.`created_at` DESC";
     const [result] = await db.query(sql);
     return result;
 };
